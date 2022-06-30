2c3de650ec510f8204189514ed0b05896822b2d6
Sébastien Braun <sebbraun@gmail.com>
2022-06-30 14:43:48 +0200

    output ec2 public IP

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,7 @@ output "private_subnets" {
   description = "List of IDs of private subnets"
   value       = ["${module.vpc.private_subnets}"]
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.lab_ec2.*.public_ip
+}
