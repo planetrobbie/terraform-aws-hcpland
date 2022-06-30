a99344570109fcf804cbf944a5936ef89a6920c4
Sébastien Braun <sebbraun@gmail.com>
2022-06-30 18:02:25 +0200

    enabling IPv6

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
-  enable_ipv6 = false
+  enable_ipv6 = true
 
   enable_nat_gateway = true
   single_nat_gateway = false
