d44583a58ce9d3d85f445c429ca48559820df660
Sébastien Braun <sebbraun@gmail.com>
2022-07-05 09:30:30 +0200

    newer patched

--- outputs.tf
+++ outputs.tf
@@ -21,7 +21,7 @@ output "ec2_public_ip" {
 }
 
 output "transit_gw_id" {
-  value = module.tgw.ec2_transit_gateway_id[0]
+  value = concat(module.tgw.*.ec2_transit_gateway_id, [""])[0]
 }
 
 output "aws_ram_resource_share" {
